fac34accc90397535124c873ccf76f16a50146d1
Confuzo <abraaovld@gmail.com>
2019-06-04 01:55:40 -0300

    implemented multiple files read

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include "../include/vrp.hpp"
 
-int main(){
+int main(int argc, char const * argv[]){
 
-
-    Vrp v("data/teste.vrp");
+    std::vector<std::string> files;
+    for(int i = 1; i < argc; i++){
+        files.push_back(argv[i]);
+    }
+    std::copy(files.begin(), files.end(), std::ostream_iterator<std::string>(std::cout, " "));
+    Vrp v(files);
 }
