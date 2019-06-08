40b4015f99db05b2b3ef1bb2f01bc250c36dceb2
Confuzo <abraaovld@gmail.com>
2019-06-08 20:26:09 -0300

    general improvements

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,6 @@ int main(int argc, char const * argv[]){
     for(int i = 1; i < argc; i++){
         files.push_back(argv[i]);
     }
-    std::copy(files.begin(), files.end(), std::ostream_iterator<std::string>(std::cout, " "));
+    //std::copy(files.begin(), files.end(), std::ostream_iterator<std::string>(std::cout, " "));
     Vrp v(files);
 }
