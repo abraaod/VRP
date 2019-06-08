7f85358370dd3a5acb4d897c9380d8cf6b29d583
Confuzo <abraaovld@gmail.com>
2019-06-08 20:26:37 -0300

    scrip to execute in parallel

---
+++ script.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+./vrp data/E-n101-k14 &
+./vrp data/E-n22-k4 &
+./vrp data/E-n23-k3 &
+./vrp data/E-n30-k3 &
+./vrp data/E-n33-k4.vrp &
+./vrp data/E-n51-k5 &
+./vrp data/E-n76-k10 &
+./vrp data/E-n76-k14 &
+./vrp data/E-n76-k7 &
+./vrp data/E-n76-k8 &
