2524ec4e24fdb84e8506624a6db9a392a2154d1c
Confuzo <abraaovld@gmail.com>
2019-06-10 01:25:49 -0300

    modifing main to Clarke & Wright algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-#include "../include/vrp.hpp"
+#include "../include/vrp_CW.hpp"
 
 int main(int argc, char const * argv[]){
 
-    std::vector<std::string> files;
+    /*std::vector<std::string> files;
     for(int i = 1; i < argc; i++){
         files.push_back(argv[i]);
-    }
+    }*/
     //std::copy(files.begin(), files.end(), std::ostream_iterator<std::string>(std::cout, " "));
-    Vrp v(files);
+    vrp_CW v(argv[1]);
 }
