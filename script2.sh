a23b24bc0a211bb76e58e967b080df5612187cf2
Confuzo <abraaovld@gmail.com>
2019-06-11 23:52:41 -0300

    adding test scripts

---
+++ script2.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+./vrp data/A-n33-k6 &
+./vrp data/A-n34-k5 &
+./vrp data/A-n36-k5 &
+./vrp data/A-n37-k5 &
+./vrp data/A-n37-k6 &
+./vrp data/A-n38-k5 &
+./vrp data/A-n39-k5 &
+./vrp data/A-n39-k6 &
+./vrp data/A-n44-k6 &
+./vrp data/A-n45-k6 &
---
+++ script3.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+./vrp data/A-n45-k7 &
+./vrp data/A-n46-k7 &
+./vrp data/A-n36-k5 &
+./vrp data/A-n48-k7 &
+./vrp data/A-n53-k7 &
+./vrp data/A-n54-k7 &
+./vrp data/A-n55-k9 &
+./vrp data/A-n60-k9 &
+./vrp data/A-n61-k9 &
+./vrp data/A-n62-k8 &
+./vrp data/A-n80-k10 &
+
